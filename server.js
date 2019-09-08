a22d1713d935bf57729e54a62c9714b2e34fac22
Davenchy <firon1222@gmail.com>
2019-09-08 18:54:18 +0200

    Merge branch 'master' of https://github.com/Davenchy/live-torrent-backend
    fixed pull conflicts

--- server.js
+++ server.js
@@ -23,9 +23,7 @@ if (!module.parent) {
   const PORT = process.env.PORT || 3000;
   const env = process.env.NODE_ENV || "production";
   app.listen(PORT, () =>
-    console.log(
-      `server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
-    )
+    console.log(`server is running in ${env} mode on port ${PORT}`)
   );
 } else {
   module.exports = app;
