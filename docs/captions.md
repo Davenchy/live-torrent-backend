505260ff5822df850f4a60557325352634c29152
Davenchy <firon1222@gmail.com>
2020-03-21 23:14:32 +0200

    update captions api to proxy opensubtitles.org http files

--- docs/captions.md
+++ docs/captions.md
@@ -2,22 +2,27 @@
 
 use **The Captions API** to find captions information
 
-The Endpoint -> `/captions`
-
-The HTTP Method always **GET**
+The HTTP Method always **GET** for all endpoints
 
 It based on [OpenSubtitles.org](https://www.opensubtitles.org) project.
+
+## Search
+
+The Endpoint -> `/captions/search`
 
 - Parameters
 
 | Parameter | Type          | Required | Default | alias | Description                              | Example  |
 | --------- | ------------- | -------- | ------- | ----- | ---------------------------------------- | -------- |
-| query     | String        | true     |         | q     | keyword to be used in the search process | shazam   |
+| query     | String        | \*       |         | q     | keyword to be used in the search process | shazam   |
 | lang      | String        | false    | all     | ln    | language id of captions                  | ara      |
 | limit     | String/Number | false    | best    | l     | limit results for quality or number      | best     |
-| imdbid    | String/Number | false    |         | im    | find captions with movie's IMDBID        | tt528809 |
+| imdbid    | String/Number | \*       |         | im    | find captions with movie's IMDBID        | tt528809 |
+| fps       | String        | false    |         | f     | number of frames per sec in the video    | 23.96    |
 | season    | Number        | false    |         | s     | find captions for series's season        | 2        |
 | episode   | Number        | false    |         | e     | find captions for series's episode       | 3        |
+
+> Required one of two parameters 'query' or 'imdbid'
 
 > Based on [OpenSubtitles.org](https://www.npmjs.com/package/opensubtitles-api)
 
@@ -27,22 +32,42 @@ You will need to assign an OpenSubtitles user agent to the env variable **OSUA**
 
 - Caption Object Example
 
+Endpoint `/captions/search?q=shazam&ln=ara&l=1`
+
 ```json
 {
-  "en": {
-    "url": "...",
-    "langcode": "en",
-    "downloads": 105677,
-    "lang": "English",
-    "encoding": "UTF-8",
-    "id": "1954422981",
-    "filename": "X-Men.Days.of.Future.Past.2014.720p.BluRay.X264-AMIABLE.srt",
-    "date": "2014-10-04 16:22:07",
-    "score": 0.5,
-    "fps": 23.976,
-    "format": "srt",
-    "utf8": "...",
-    "vtt": "..."
-  }
+  "ar": [
+    {
+      "url": "...",
+      "langcode": "ar",
+      "downloads": 48488,
+      "lang": "Arabic",
+      "encoding": "UTF-8",
+      "id": "1956328649",
+      "filename": "Shazam.2019.1080p.V2.HC.HDRip.X264-EVO.srt",
+      "date": "2019-05-15 23:27:34",
+      "score": 0.5,
+      "fps": 30,
+      "format": "srt",
+      "utf8": "...",
+      "vtt": "..."
+    }
+  ]
 }
 ```
+
+## Movie
+
+The Endpoint -> `/captions/movie/:imdbid`
+
+Get caption for a movie by its IMDBID in vtt format
+
+- Parameters
+
+| Parameter | Type    | Required | Default | alias | Description                                        | Example |
+| --------- | ------- | -------- | ------- | ----- | -------------------------------------------------- | ------- |
+| lang      | String  | false    | eng     | l     | language id of captions                            | ara     |
+| fps       | String  | false    |         | f     | number of frames per sec in the video              | 23.96   |
+| allow_srt | Boolean | false    | false   | s     | if caption format srt then don't convert it to vtt | true    |
+
+The result type can be `application/x-subrip` for `.srt` files or `text/vtt` for `.vtt` files
--- routes/captions.js
+++ routes/captions.js
@@ -1,5 +1,12 @@
 const app = require("express")();
 const OS = require("opensubtitles-api");
+const axios = require("axios").default;
+const encodingDetector = require("jschardet");
+const iconv = require("iconv-lite");
+const { Readable } = require("stream");
+const srt2vtt = require("srt-to-vtt");
+
+// console.log(typeof formatConverter);
 
 const OpenSubtitles = new OS(process.env.OSUA || "TemporaryUserAgent");
 let isLoggedIn = null;
@@ -28,14 +35,15 @@ app.use(
   }
 );
 
-app.get("/", (req, res) => {
+app.get("/search", (req, res) => {
   const rq = req.query,
     sublanguageid = rq.lang || rq.ln || "all",
     query = rq.query || rq.q,
     limit = rq.limit || rq.l || "best",
     season = rq.season || rq.s,
     episode = rq.episode || rq.e,
-    imdbid = rq.imdbid || rq.im;
+    imdbid = rq.imdbid || rq.im,
+    fps = rq.fps || rq.f;
 
   OpenSubtitles.search({
     sublanguageid,
@@ -43,7 +51,9 @@ app.get("/", (req, res) => {
     limit,
     season,
     episode,
-    imdbid
+    imdbid,
+    fps,
+    extensions: ["srt", "vtt"]
   })
     .then(data => res.send(data))
     .catch(err => {
@@ -51,5 +61,105 @@ app.get("/", (req, res) => {
       res.status(400).send(err.message || "try again later");
     });
 });
+
+const loadMovieData = (req, res, next) => {
+  const { query: q, params: p } = req;
+  req.movie = {
+    id: p.imdbid,
+    lang: q.lang || q.l || "eng",
+    fps: q.fps || q.f,
+    srt: q.allow_srt || q.s
+  };
+  next();
+};
+
+const movieCaption = (req, res, next) => {
+  const { id, lang, fps } = req.movie;
+
+  OpenSubtitles.search({
+    sublanguageid: lang,
+    imdbid: id,
+    limit: 1,
+    extensions: ["srt", "vtt"],
+    fps
+  })
+    .then(data => {
+      const captionLangObj = Object.values(data)[0];
+      if (!captionLangObj) return res.sendStatus(404);
+      const captionData = captionLangObj[0];
+      if (!captionData) return res.sendStatus(404);
+      const { format: f } = captionData;
+      if (f !== "srt" && f !== "vtt") return res.sendStatus(404);
+      req.movie.caption = captionData;
+      next();
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(400).send(err.message || "try again later");
+    });
+};
+
+const processCaption = (req, res, next) => {
+  const { caption, srt } = req.movie;
+  axios
+    .get(caption.utf8)
+    .then(response => {
+      let encoding = caption.encoding;
+      let data = response.data;
+
+      // detect encoding
+      if (!encoding) encoding = encodingDetector.detect(data).encoding;
+      if (!encoding)
+        return res.status(500).send("cannot detect caption encoding");
+
+      // convert caption data encoding to UTF-8 if it is not
+      if (encoding !== "UTF-8") {
+        const decodedData = iconv.decode(data, encoding);
+        data = iconv.encode(decodedData, "utf8").toString();
+      }
+
+      // convert srt to vtt format
+      if (caption.format === "srt" && !srt) {
+        const stream = new Readable();
+        stream._read = () => {};
+        stream.push(data);
+        stream.on("data", chunk => {
+          req.movie.caption.data = chunk;
+          req.movie.caption.format = "vtt";
+          req.movie.caption.filename = req.movie.caption.filename.replace(
+            ".srt",
+            ".vtt"
+          );
+          next();
+        });
+        stream.pipe(srt2vtt());
+      } else {
+        req.movie.caption.data = data;
+        next();
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send(err.message || "cannot download the caption file");
+    });
+};
+
+const sendCaptions = (req, res) => {
+  const { caption } = req.movie;
+  res.attachment(caption.filename);
+  res.setHeader("Content-Length", caption.data.length);
+  res.setHeader("Content-Type", "text/vtt");
+  req.connection.setTimeout(30000);
+  console.log("sending");
+  res.send(caption.data);
+};
+
+app.get(
+  "/movie/:imdbid",
+  loadMovieData,
+  movieCaption,
+  processCaption,
+  sendCaptions
+);
 
 module.exports = app;
