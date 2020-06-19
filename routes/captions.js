39428e40ee4a4e379a4dcb9e9f177b0d444c5211
Davenchy <firon1222@gmail.com>
2020-06-19 17:55:13 +0200

    update captions route

--- routes/captions.js
+++ routes/captions.js
@@ -1,42 +1,23 @@
 const app = require("express")();
-const OSAPI = require("opensubtitles-api");
-const axios = require("axios").default;
-const encodingDetector = require("jschardet");
-const iconv = require("iconv-lite");
-const srt2vtt = require("srt-to-vtt");
-const isoCodes = require("iso-language-codes");
-const { Readable } = require("stream");
-
-// console.log(typeof formatConverter);
-
-const api = new OSAPI(process.env.OSUA || "TemporaryUserAgent");
-let isLoggedIn = null;
+const service = require("../services/captions");
+const { CustomError } = require("../helpers/errors");
 
 // is logged in middleware
 app.use(
-  (req, res, next) => {
-    if (isLoggedIn === null) {
-      api
-        .login()
-        .then(() => {
-          isLoggedIn = true;
-          console.log("OpenSubtitles.org: LoggedIn");
-          next();
-        })
-        .catch(err => {
-          console.error("OpenSubtitles.org: Login Failed!!");
-          console.error(err);
-          next();
-        });
-    } else next();
+  // @ts-ignore
+  async (req, res, next) => {
+    if (!service.isLoggedIn) await service.login();
+    next();
   },
+  // @ts-ignore
   (req, res, next) => {
-    if (!isLoggedIn) res.status(500).send("OpenSubtitles.org Login Failed!");
-    else next();
+    if (!service.isLoggedIn)
+      throw new CustomError(500, "OpenSubtitles.org Login Failed!");
+    next();
   }
 );
 
-app.get("/search", (req, res) => {
+app.get("/search", async (req, res) => {
   const rq = req.query;
   const sublanguageid = rq.lang || rq.ln || "all";
   const query = rq.query || rq.q;
@@ -46,24 +27,29 @@ app.get("/search", (req, res) => {
   const imdbid = rq.imdbid || rq.im;
   const fps = rq.fps || rq.f;
 
-  api
-    .search({
-      sublanguageid,
-      query,
-      limit,
-      season,
-      episode,
-      imdbid,
-      fps,
-      extensions: ["srt", "vtt"]
-    })
-    .then(data => res.send(data))
-    .catch(err => {
-      console.error(err);
-      res.status(400).send(err.message || "try again later");
-    });
+  const results = await service.search({
+    // @ts-ignore
+    sublanguageid,
+    // @ts-ignore
+    query,
+    // @ts-ignore
+    limit,
+    // @ts-ignore
+    season,
+    // @ts-ignore
+    episode,
+    // @ts-ignore
+    imdbid,
+    // @ts-ignore
+    fps,
+    extensions: ["srt", "vtt"]
+  });
+
+  res.send(results);
 });
 
+// load movie's data
+// @ts-ignore
 const loadMovieData = (req, res, next) => {
   const { query: q, params: p } = req;
   req.movie = {
@@ -75,79 +61,24 @@ const loadMovieData = (req, res, next) => {
   next();
 };
 
-const movieCaption = (req, res, next) => {
+// find movie's caption
+// @ts-ignore
+const movieCaption = async (req, res, next) => {
   const { id, lang, fps } = req.movie;
-
-  api
-    .search({
-      sublanguageid: lang,
-      imdbid: id,
-      limit: 1,
-      extensions: ["srt", "vtt"],
-      fps
-    })
-    .then(data => {
-      const captionLangObj = Object.values(data)[0];
-      if (!captionLangObj) return res.sendStatus(404);
-      const captionData = captionLangObj[0];
-      if (!captionData) return res.sendStatus(404);
-      const { format: f } = captionData;
-      if (f !== "srt" && f !== "vtt") return res.sendStatus(404);
-      req.movie.caption = captionData;
-      next();
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(400).send(err.message || "try again later");
-    });
+  const caption = await service.findMovieCaption(id, lang, fps);
+  req.movie.caption = caption;
+  next();
 };
 
-const processCaption = (req, res, next) => {
-  const { caption, srt } = req.movie;
-  axios
-    .get(caption.utf8)
-    .then(response => {
-      let encoding = caption.encoding;
-      let data = response.data;
-
-      // detect encoding
-      if (!encoding) encoding = encodingDetector.detect(data).encoding;
-      if (!encoding) {
-        return res.status(500).send("cannot detect caption encoding");
-      }
-
-      // convert caption data encoding to UTF-8 if it is not
-      if (encoding !== "UTF-8") {
-        const decodedData = iconv.decode(data, encoding);
-        data = iconv.encode(decodedData, "utf8").toString();
-      }
-
-      // convert srt to vtt format
-      if (caption.format === "srt" && !srt) {
-        const stream = new Readable();
-        stream._read = () => {};
-        stream.push(data);
-        stream.on("data", chunk => {
-          req.movie.caption.data = chunk;
-          req.movie.caption.format = "vtt";
-          req.movie.caption.filename = req.movie.caption.filename.replace(
-            ".srt",
-            ".vtt"
-          );
-          next();
-        });
-        stream.pipe(srt2vtt());
-      } else {
-        req.movie.caption.data = data;
-        next();
-      }
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(500).send(err.message || "cannot download the caption file");
-    });
+// download and process the caption
+// @ts-ignore
+const processCaption = async (req, res, next) => {
+  const movie = req.movie;
+  req.movie.caption = await service.downloadCaption(movie.caption, !!movie.srt);
+  next();
 };
 
+// send caption
 const sendCaptions = (req, res) => {
   const { caption } = req.movie;
   res.attachment(caption.filename);
@@ -158,37 +89,7 @@ const sendCaptions = (req, res) => {
   res.send(caption.data);
 };
 
-// get all supported languages
-app.get("/movie/:imdbid/langs", (req, res) => {
-  const id = req.params.imdbid;
-  api
-    .search({
-      imdbid: id,
-      limit: "best"
-    })
-    .then(data => {
-      const langs = Object.keys(data).map(a => {
-        const d = isoCodes.by639_1[a];
-        const b = data[a];
-
-        if (d) {
-          return {
-            name: d.name,
-            code: d.iso639_1,
-            iso: d.iso639_2B
-          };
-        } else {
-          return {
-            name: b.lang,
-            code: b.langcode,
-            iso: b.langcode
-          };
-        }
-      });
-      res.send(langs);
-    });
-});
-
+// get movie caption
 app.get(
   "/movie/:imdbid",
   loadMovieData,
@@ -196,5 +97,12 @@ app.get(
   processCaption,
   sendCaptions
 );
+
+// get all supported languages
+app.get("/movie/:imdbid/langs", async (req, res) => {
+  const id = req.params.imdbid;
+  const langs = await service.getSupportedLanguages(id);
+  res.send(langs);
+});
 
 module.exports = app;
