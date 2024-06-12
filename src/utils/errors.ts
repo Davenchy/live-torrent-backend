243b285a345bbbbaa109af5fe94450d2360fb0c7
Fadi Asaad <firon1222@gmail.com>
2024-06-12 19:19:51 +0300

    update(errors): define predefined errors and update the HTTPException class

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,46 +1,81 @@
 import type { ErrorHandler } from 'hono/types'
 import type { StatusCode } from 'hono/utils/http-status'
 
+export interface HTTPExceptionOptions {
+  code?: number
+  error?: Error
+}
+
 export class HTTPException extends Error {
   constructor(
-    public status: StatusCode,
     message: string,
-    public error: Error | undefined = undefined,
+    public status: StatusCode = 500,
+    public options: HTTPExceptionOptions = {},
   ) {
     super(message)
-  }
-
-  static fromGeneral(error: Error): HTTPException {
-    const e = new HTTPException(500, `GeneralError: ${error.message}`, error)
-    e.name = error.name
-    return e
+    this.stack = new Error().stack
   }
 
   toString() {
-    return `HTTPException:${this.name}(${this.status}): ${this.message}
-
-Error: ${this.error}
-
+    const { options, status, message, name } = this
+    return `\nError HTTPException(${name}):(status: ${status}, code: ${options.code ?? -1}): ${message}
+${options.error ? options.error : ''}
 Stack: ${this.stack}
-
-Cause: ${this.cause}`
+${this.cause ? `Cause: ${this.cause}` : ''}`
   }
 
   toJSON() {
     return {
-      status: 'error',
-      statusCode: this.status,
-      message: this.message,
+      code: this.options.code ?? -1,
+      error: this.message,
     }
   }
 }
 
 export const HTTPExceptionErrorHandler: ErrorHandler = (err, c) => {
   let error: HTTPException
+  console.log('error', err)
 
   if (err instanceof HTTPException) error = err
-  else error = HTTPException.fromGeneral(err)
+  else {
+    error = new HTTPException(err.message, 500, { code: -1, error: err })
+    error.name = err.name
+    error.stack = err.stack
+  }
 
   console.error(error.toString())
   return c.json(error.toJSON(), error.status)
 }
+
+export const TorrentInvalidError = (
+  error: Error | undefined = undefined,
+): HTTPException =>
+  new HTTPException('Required valid id query or hash param', 400, {
+    error,
+    code: 1,
+  })
+
+export const NoSelectionError = (
+  error: Error | undefined = undefined,
+): HTTPException =>
+  new HTTPException(
+    'File selection is required, use query: q or i to select one.',
+    400,
+    { error, code: 2 },
+  )
+
+export const FileNotFoundError = (
+  error: Error | undefined = undefined,
+): HTTPException =>
+  new HTTPException('File Not Found!', 404, { code: 0, error })
+
+export const RequiredSearchQueryError = (
+  error: Error | undefined = undefined,
+): HTTPException =>
+  new HTTPException('query is required!', 400, { error, code: 4 })
+
+export const UnsupportedProviderError = (
+  provider: string,
+  error: Error | undefined = undefined,
+): HTTPException =>
+  new HTTPException(`Unsupported ${provider} provider`, 400, { code: 5, error })
