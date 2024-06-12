8231b144a88becd14c70c72bbfb1cbe98e89f0cd
Fadi Asaad <firon1222@gmail.com>
2024-06-12 19:19:04 +0300

    update(index.ts): add /docs endpoint

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,22 @@ import { Hono } from 'hono'
 import { serve } from '@hono/node-server'
 import { swaggerUI } from '@hono/swagger-ui'
 import { HTTPExceptionErrorHandler } from 'src/utils/errors'
+import YAML from 'yamljs'
 
 import routes from 'routes/index'
 
 const app = new Hono()
 
 app.route('/', routes)
+app.get('/', c => c.redirect('/docs'))
 app.get('/ping', c => c.text('pong'))
-app.get('/docs', swaggerUI({ url: '/docs' }))
+app.get(
+  '/docs',
+  swaggerUI({
+    url: 'openapi.yaml',
+    spec: YAML.load('./openapi.yaml'),
+  }),
+)
 
 app.onError(HTTPExceptionErrorHandler)
 
